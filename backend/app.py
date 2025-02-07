978658bd8ef9b98ce78c192bddeca0e2fb7b2f87
lbugata <bchennupalli@binghamton.edu>
2025-02-07 18:46:37 -0500

    live app first commit

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, send_from_directory
 from flask_cors import CORS
 import os
 
@@ -7,7 +7,7 @@ from predictor import Predictor
 from image_processor import ImageProcessor
 from device_manager import DeviceManager
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend/static", template_folder="../frontend")
 CORS(app)
 
 UPLOAD_FOLDER = 'uploads'
@@ -18,6 +18,10 @@ model_loader = ModelLoader('model.pth', device)
 model = model_loader.get_model()
 image_processor = ImageProcessor()
 predictor = Predictor(model, device)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
@@ -34,6 +38,10 @@ def upload_image():
     result = "AI Generated Image" if prediction == 1 else "Human Captured Image"
     print(f"Prediction: {result}, Probability: {probability:.4f}")
     return jsonify({"result": result, "probability": f"{probability:.4f}"})
+
+@app.route('/uploads/<path:path>')
+def serve_static(path):
+    return send_from_directory("../frontend/static", path)
 
 if __name__ == "__main__":
     app.run(debug=True)
